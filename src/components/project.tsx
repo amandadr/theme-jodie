9877f87a40142925f301daecc9090e1e2098a22a
amandadr <amandadroy@gmail.com>
2023-12-16 15:31:40 -0400

    Revert "whoopsie take back project changes"
    This reverts commit ece87c2a1f18cd41c43bafc51750a89ca121e576.

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,5 +1,4 @@
 /** @jsx jsx */
-/** @jsxFrag */
 import { jsx, Heading } from "theme-ui"
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
@@ -10,62 +9,63 @@ import Seo from "./seo"
 
 export type JodieProjectProps = {
   project: {
-    excerpt: string
-    color: string
-    date: string
-    slug: string
-    title: string
-    shortTitle: string
-    category: string
+    excerpt: string;
+    color: string;
+    date: string;
+    slug: string;
+    title: string;
+    shortTitle: string;
+    category: string;
     cover: {
       childImageSharp: {
         resize: {
-          src: string
-        }
-      }
-    }
-  }
+          src: string;
+        };
+      };
+    };
+  };
   images: {
     nodes: {
-      name: string
+      name: string;
       childImageSharp: {
-        gatsbyImageData: IGatsbyImageData
-      }
-    }[]
-  }
-}
+        gatsbyImageData: IGatsbyImageData;
+      };
+    }[];
+  };
+};
 
-const Project: React.FC<
-  React.PropsWithChildren<PageProps<JodieProjectProps>>
-> = ({ data: { project, images }, children }) => (
-  <>
-    <Layout >
-      <div sx={{ variant: `content.project` }}>
-        <div
-          sx={{
-            fontSize: 2,
-            textTransform: `uppercase`,
-            letterSpacing: `wider`,
-            mb: 2,
-          }}
-        >
-        </div>
-        <Heading as="h1" variant="styles.h1" sx={{ mt: 0 }}>
-          {project.title}
-        </Heading>
-        <div sx={{ maxWidth: `70ch`, my: 4 }}>{children}</div>
+const Project: React.FC<React.PropsWithChildren<PageProps<JodieProjectProps>>> = ({
+  data: { project, images },
+  children,
+}) => (
+  <Layout color={project.color || undefined}>
+    <div sx={{ variant: `content.project` }}>
+      <div sx={{ fontSize: 2, textTransform: `uppercase`, letterSpacing: `wider`, mb: 2 }}>{project.category}</div>
+      <Heading as="h1" variant="styles.h1" sx={{ mt: 0 }}>
+        {project.title}
+      </Heading>
+      <div sx={{ maxWidth: `70ch`, my: 4 }}>{children}</div>
+    </div>
+    <div sx={{ backgroundColor: transparentize(0.9, project.color) }}>
+      <div sx={{ variant: `content.imageList` }}>
+        {images.nodes.map((image) => (
+          <GatsbyImage key={image.name} alt={image.name} image={image.childImageSharp.gatsbyImageData} />
+        ))}
       </div>
-    </Layout>
-  </>
-);
+    </div>
+  </Layout>
+)
 
-export default Project
+export default Project;
 
-export const Head: HeadFC<JodieProjectProps> = ({ data: { project }, location }) => (
+export const Head: HeadFC<JodieProjectProps> = ({
+  data: { project },
+  location,
+}) => (
   <Seo
     title={project.title}
     description={project.excerpt}
     pathname={location.pathname}
     image={project.cover.childImageSharp.resize.src}
   />
-)
+);
